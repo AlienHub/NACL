ce699a45509e36a2db139fdd0a867c012cd56c41
Alien <zhouxshh@163.com>
2024-03-07 17:07:15 +0800

    dev:pwa配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+import { pwa } from './config/pwa'
+import { appDescription } from './constants/index'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -5,7 +8,22 @@ export default defineNuxtConfig({
     // prerender index route by default
     '/': { prerender: true },
   },
-  modules: ['@element-plus/nuxt'],
+  modules: ['@element-plus/nuxt','@vite-pwa/nuxt'],
   css: ['~/assets/main.css'],
   ssr: false,
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no' },
+        { name: 'description', content: appDescription },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
+  },
+  pwa
 });
